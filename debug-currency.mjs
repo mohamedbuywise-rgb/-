4ba076d0c4ed2d25445fe77704a4676d9b632d61
mohamedbuywise-rgb <301648295+mohamedbuywise-rgb@users.noreply.github.com>
2026-09-03 20:18:02 +0300

    commit

--- debug-currency.mjs
+++
@@ -1,4 +0,0 @@
-import { detectCurrency } from './lib/textNormalize.js';
-for (const text of ['اشتريت موبايل ب 4000 جنيه', 'ربحت 300 دولار', 'I spent 50 USD on food', '300 جنيه']) {
-  console.log(text, detectCurrency(text));
-}
